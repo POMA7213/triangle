6aa5e1ad84d6df270cd4b10e3df30354f5519800
Roman <romannaumovrn@gmail.com>
2022-06-29 17:14:30 +0300

    first com

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Globalization;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint h = uint.Parse(Console.ReadLine());
+            for (uint i = 0; i < h; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write("#");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            for (uint i = h; i > 0; i--)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write("#");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            for (uint i = h; i > 0; i--)
+            {
+                int j = 0;
+                for (; j < i-1; j++)
+                {
+                    Console.Write(" ");
+                }
+                for(;j<h; j++)
+                {
+                    Console.Write("#");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            for (uint i = 0; i < h; i++)
+            {
+                int j = 0;
+                for (; j < i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (; j < h; j++)
+                {
+                    Console.Write("#");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
